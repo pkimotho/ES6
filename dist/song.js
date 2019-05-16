d969e3378b0b344162064e0d8215e1e6ebb5769c
Kimotho <plkimotho@gmail.com>
2019-05-16 14:36:06 +0300

    adds javascript variables

---
+++ dist/song.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/* SONG.JS
+Contains details of my favorite song assigned into javascript variables
+*/
+var songName = 'Melanin';
+var genre = 'Afro-pop'; // Assigned Using a date object
+
+var releaseDate = new Date('November, 27, 2017');
+var durationInSeconds = 300;
+var album = 'Afrikan Sauce';
+var artist = 'Sauti Sol';
+var cityOfOrigin = 'Nairobi';
+var countryOfOrigin = 'Kenya';
+console.log(songName);
+console.log(genre);
+console.log(releaseDate);
+console.log(durationInSeconds);
+console.log(album);
+console.log(artist);
+console.log(cityOfOrigin);
+console.log(countryOfOrigin);
